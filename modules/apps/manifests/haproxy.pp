cf9b303f10d23ca670d7a0bc4fc715a27d7b3ddc
Christian Jellinghaus <chscde@gmail.com>
2015-01-15 21:55:24 +0100

    haproxy 1.5

--- modules/apps/manifests/haproxy.pp
+++ modules/apps/manifests/haproxy.pp
@@ -1,12 +1,22 @@
 class apps::haproxy {
+	exec { "apt-get update2" :
+		command => "/usr/bin/apt-get update",
+	}
+
+	exec { 'add haproxy repo':
+		command => 'apt-add-repository ppa:vbernat/haproxy-1.5',
+		path => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+		require => Exec["apt-get update2"],
+	}
+
 	package { "haproxy" :
 		ensure => installed,
-		require => [Class["base::apt-get-update"], Exec["configure locale"]],
+		require => [Class["base::apt-get-update"], Exec["configure locale"], Exec["add haproxy repo"]],
 	}
 
 	exec { 'enable haproxy':
-		command      => "sed -i /etc/default/haproxy -e 's/ENABLED=0/ENABLED=1/'",
-		path        => '/bin',
-		require    => Package["haproxy"],
+		command => "sed -i /etc/default/haproxy -e 's/ENABLED=0/ENABLED=1/'",
+		path => '/bin',
+		require => Package["haproxy"],
 	}
 }
--- modules/base/manifests/apt-get-update.pp
+++ modules/base/manifests/apt-get-update.pp
@@ -2,4 +2,9 @@ class base::apt-get-update {
 	exec { "apt-get update" :
 		command => "/usr/bin/apt-get update",
 	}
+
+	package { 'python-software-properties':
+		ensure => installed,
+		require => Exec["apt-get update"],
+	}
 }
